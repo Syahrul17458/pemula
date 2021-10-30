14a16cf32148d4020febeff38083efc60e6f7515
Syahrul17458 <83635606+Syahrul17458@users.noreply.github.com>
2021-10-30 19:50:03 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,8 +4,8 @@ let handler = function (m) {
   for (let owner of Object.entries(global.Owner).filter(v => v[1].isCreator)) {
     contacts.push(...[owner[0], owner[1].name])
   }
-  if (contacts.length < 3) return this.sendContact(m.chat, contacts[0], contacts[1], m)
-  this.sendContactArray(m.chat, contacts, m)
+  if (contacts.length < 3) return this.sendContact(m.chat, '6285926431562', 'Rulzt', m)
+  this.sendContactRulzt(m.chat, '6285926431562', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
